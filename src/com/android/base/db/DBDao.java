e8e66c2f4d961f4ea6fb412ad3702536d38c1c07
饶涛 <raotao@cmstop.com>
2015-10-23 16:02:05 +0800

    android base
    android base

---
+++ src/com/android/base/db/DBDao.java
@@ -0,0 +1,262 @@
+package com.android.base.db;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.android.base.db.annotation.DBField;
+import com.android.base.db.annotation.DBTableName;
+import com.android.base.utils.StringUtils;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * 数据库基础操作DAO
+ * 
+ * @author krubo
+ *
+ */
+public class DBDao<T> {
+
+	protected SQLiteDatabase db;
+	protected String table;
+	protected Class<T> cls;
+
+	public DBDao(Class<T> cls) {
+		super();
+		this.db = DBManager.getInstance().getDb();
+		this.cls = cls;
+		this.table = this.cls.getAnnotation(DBTableName.class).tableName();
+	}
+
+	/**
+	 * 获取ContentVlues
+	 * 
+	 * @param t
+	 * @return
+	 */
+	protected ContentValues getContentVlues(T t) {
+		Field[] fields = t.getClass().getDeclaredFields();
+		Field.setAccessible(fields, true);
+		ContentValues values = new ContentValues();
+		for (Field field : fields) {
+			if (field.getAnnotation(DBField.class) != null) {
+				try {
+					Object value = field.get(t);
+					values.put(field.getName(), value == null ? "" : value + "");
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return values;
+	}
+
+	/**
+	 * 插入单条数据
+	 * 
+	 * @param t
+	 */
+	public void insert(T t) {
+		if (db != null && t != null) {
+			ContentValues values = getContentVlues(t);
+			db.insert(table, null, values);
+		}
+	}
+
+	/**
+	 * 插入多条数据
+	 * 
+	 * @param list
+	 */
+	public void insert(List<T> list) {
+		if (list == null) {
+			return;
+		}
+		for (T t : list) {
+			insert(t);
+		}
+	}
+
+	/**
+	 * 修改单条数据
+	 * 
+	 * @param t
+	 * @param whereClause
+	 * @param whereArgs
+	 */
+	public void update(T t, String whereClause, String[] whereArgs) {
+		if (db != null && t != null) {
+			ContentValues values = getContentVlues(t);
+			db.update(table, values, whereClause, whereArgs);
+		}
+	}
+
+	/**
+	 * 根据条件取出数据
+	 * 
+	 * @param selection
+	 * @param selectionArgs
+	 * @param groupBy
+	 * @param having
+	 * @param orderBy
+	 * @return
+	 */
+	public List<T> query(String selection, String[] selectionArgs, String groupBy, String having, String orderBy) {
+		List<T> list = new ArrayList<>();
+		if (db != null) {
+			Cursor cursor = null;
+			try {
+				cursor = db.query(table, null, selection, selectionArgs, groupBy, having, orderBy);
+				while (cursor.moveToNext()) {
+					T t = cls.newInstance();
+					if (t == null) {
+						continue;
+					}
+					Field[] fields = t.getClass().getDeclaredFields();
+					Field.setAccessible(fields, true);
+					for (Field field : fields) {
+						if (field.getAnnotation(DBField.class) != null) {
+							String type = field.getType().toString();
+							String value = cursor.getString(cursor.getColumnIndex(field.getName()));
+							if (type.endsWith("String")) {
+								field.set(t, value + "");
+							} else if (type.endsWith("boolean") || type.endsWith("Boolean")) {
+								field.setBoolean(t, (value == null || value.equals("false") ? false : true));
+							} else if (type.endsWith("int") || type.endsWith("Integer")) {
+								field.setInt(t, StringUtils.isEmptyByTrim(value) ? 0 : Integer.valueOf(value));
+							} else if (type.endsWith("float") || type.endsWith("Float")) {
+								field.setFloat(t, StringUtils.isEmptyByTrim(value) ? 0 : Float.valueOf(value));
+							} else if (type.endsWith("double") || type.endsWith("Double")) {
+								field.setDouble(t, StringUtils.isEmptyByTrim(value) ? 0 : Double.valueOf(value));
+							} else if (type.endsWith("long") || type.endsWith("Long")) {
+								field.setLong(t, StringUtils.isEmptyByTrim(value) ? 0 : Long.valueOf(value));
+							} else if (type.endsWith("short") || type.equals("Short")) {
+								field.setShort(t, StringUtils.isEmptyByTrim(value) ? 0 : Short.valueOf(value));
+							}
+						}
+					}
+					list.add(t);
+				}
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (cursor != null) {
+				cursor.close();
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * 取出所有数据
+	 * 
+	 * @return
+	 */
+	public List<T> query() {
+		return query(null, null, null, null, null);
+	}
+
+	/**
+	 * 删除所有数据
+	 */
+	public void deleteAll() {
+		if (db != null) {
+			db.delete(table, null, null);
+		}
+	}
+
+	/**
+	 * 根据条件删除数据
+	 * 
+	 * @param whereClause
+	 * @param whereArgs
+	 */
+	public void delete(String whereClause, String[] whereArgs) {
+		if (db != null) {
+			db.delete(table, whereClause, whereArgs);
+		}
+	}
+
+	/**
+	 * 根据实体类删除数据
+	 * 
+	 * @param t
+	 */
+	public void delete(T t) {
+		if (t == null) {
+			return;
+		}
+		Field[] fields = t.getClass().getDeclaredFields();
+		Field.setAccessible(fields, true);
+		String whereClause = "";
+		List<String> whereArgs = new ArrayList<String>();
+		boolean isFirst = true;
+		for (Field field : fields) {
+			if (field.getAnnotation(DBField.class) != null) {
+				try {
+					Object value = field.get(t);
+					if (isFirst) {
+						whereClause = field.getName() + "=?";
+						isFirst = false;
+					} else {
+						whereClause = whereClause + " and " + field.getName() + "=?";
+					}
+					whereArgs.add(value == null ? "" : value + "");
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		if (whereArgs.isEmpty()) {
+			return;
+		}
+		delete(whereClause, (String[]) whereArgs.toArray(new String[0]));
+	}
+
+	/**
+	 * 删除当前表
+	 */
+	public void dropTable() {
+		if (db != null) {
+			String sql = "DROP TABLE " + table;
+			db.execSQL(sql);
+		}
+	}
+
+	/**
+	 * 检查表是否为空
+	 * 
+	 * @return true 为空
+	 */
+	public boolean isEmptyTable() {
+		// 返回true为空，
+		boolean flag = true;
+		if (db != null) {
+			Cursor cursor = db.query(table, null, null, null, null, null, null);
+			int x = cursor.getCount();
+			cursor.close();
+			if (x == 0) {
+				flag = true;
+			} else {
+				flag = false;
+			}
+		}
+		return flag;
+	}
+
+}
---
+++ src/com/android/test/PersonDao.java
@@ -0,0 +1,12 @@
+package com.android.test;
+
+import com.android.base.db.DBDao;
+
+public class PersonDao extends DBDao<Person> {
+
+	public PersonDao(Class<Person> cls) {
+		super(cls);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
